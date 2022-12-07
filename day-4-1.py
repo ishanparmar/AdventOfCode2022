6a095da88efc362f1ace05e18e1ae847130ba113
Ishan Parmar <ipparmar@amazon.com>
2022-12-07 13:40:06 -0500

    minor refactoring day-4-1 https://adventofcode.com/2022/day/4/answer

--- day-4-1.py
+++ day-4-1.py
@@ -1,7 +1,6 @@
-f = open("day-4-input", "r")
 count: int = 0
-for l in f:
-    ranges = l.rstrip().split(",")
+for line in open("day-4-input", "r"):
+    ranges = line.rstrip().split(",")
     if (int(ranges[0].split("-")[0]) >= int(ranges[1].split("-")[0]) and int(ranges[0].split("-")[1]) <= int(ranges[1].split("-")[1])) or \
             (int(ranges[0].split("-")[0]) <= int(ranges[1].split("-")[0]) and int(ranges[0].split("-")[1]) >= int(ranges[1].split("-")[1])):
         count += 1
